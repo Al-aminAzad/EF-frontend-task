1bcf0b9b3987b192a5e90e5801614f55e061e9d9
al-amin <alamin@priyo.com>
2023-08-19 23:16:21 +0600

    search bar and child menu click functionalites added

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+const roundInput = document.getElementById('search-input');
+const pcItem = document.getElementById('pc');
+const playstation = document.getElementById('playstation');
+const xbox = document.getElementById('xbox');
+const nintendo = document.getElementById('nintendo')
+const navbar = document.querySelector('.navbar');
+const childBlock = document.getElementById('childMenu');
+roundInput.addEventListener('focus', () => {
+  navbar.style.display = 'none';
+});
+
+roundInput.addEventListener('blur', () => {
+  navbar.style.display = 'flex';
+});
+function toggleChildBlock(isVisible) {
+  if (isVisible) {
+    childBlock.style.display = 'none';
+  } else {
+    childBlock.style.display = 'flex';
   }
+}
+let pcBlockVisible = false;
+let playstationVisible = false;
+let xboxVisible = false;
+let nintendoVisible = false;
+
+
+pcItem.addEventListener('click', () => {
+  toggleChildBlock(pcBlockVisible);
+  pcBlockVisible = !pcBlockVisible;
+});
+playstation.addEventListener('click', () => {
+  toggleChildBlock(playstationVisible);
+  playstationVisible = !playstationVisible;
+});
+
+xbox.addEventListener('click', () => {
+  toggleChildBlock(xboxVisible);
+  xboxVisible = !xboxVisible;
+});
+
+nintendo.addEventListener('click', () => {
+  toggleChildBlock(nintendoVisible);
+  nintendoVisible = !nintendoVisible;
+});
+
+
+
+
+
